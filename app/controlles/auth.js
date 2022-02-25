e41b31b8bd82ee9c8ddc5dbcd268b8cd760aab68
Juan Camilo salazar osorio <juanc.salazaro@autonoma.edu.co>
2022-02-24 22:58:19 -0500

    Api Spotify

---
+++ app/controlles/auth.js
@@ -0,0 +1,73 @@
+const { httpError } = require('../helpers/handleError')
+const { encrypt, compare } = require('../helpers/handleBcrypt')
+const { tokenSign } = require('../helpers/generateToken')
+const userModel = require('../models/users')
+
+//TODO: Login!
+const loginCtrl = async(req, res) => {
+    try {
+
+        const mockUser = {
+            name: 'Camilo',
+            email: 'camilo@spotify.com',
+            password: '12345678',
+            avatar: 'https://static.wikia.nocookie.net/houseofmouse20/images/c/c8/Perry-456x255.jpg/revision/latest/top-crop/width/360/height/450?cb=20190410180839&path-prefix=es'
+        }
+
+        const { email, password } = req.body
+
+
+        if (mockUser.email !== 'camilo@spotify.com') {
+            res.status(404)
+            res.send({ error: 'User not found' })
+        }
+
+        const checkPassword = (mockUser.password === password)
+
+        //TODO JWT 👉
+        const tokenSession = await tokenSign(mockUser) //TODO: 2d2d2d2d2d2d2
+
+        if (checkPassword) { //TODO Contraseña es correcta!
+            res.send({
+                data: mockUser,
+                tokenSession
+            })
+            return
+        }
+
+        if (!checkPassword) {
+            res.status(409)
+            res.send({
+                error: 'Invalid password'
+            })
+            return
+        }
+
+    } catch (e) {
+        httpError(res, e)
+    }
+}
+
+//TODO: Registramos usuario!
+const registerCtrl = async(req, res) => {
+    try {
+        //TODO: Datos que envias desde el front (postman)
+        const { email, password, name } = req.body
+
+        const passwordHash = await encrypt(password) //TODO: (123456)<--- Encriptando!!
+        const registerUser = await userModel.create({
+            email,
+            name,
+            password: passwordHash
+        })
+
+        res.send({ data: registerUser })
+
+    } catch (e) {
+        httpError(res, e)
+    }
+}
+
+
+
+module.exports = { loginCtrl, registerCtrl }
